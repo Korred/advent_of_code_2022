8f9e64c5bd56aceb895d9d6319fdf1da2f7d4c81
Philipp Kochanski <kochanskip@outlook.com>
2022-12-05 07:15:35 +0100

    Solution for Day 5

---
+++ day_05/task_1.py
@@ -0,0 +1,36 @@
+from collections import deque
+import re
+
+
+def parse_supply_input(path):
+    stack_input, moves_input = map(
+        lambda x: x.split("\n"), open(path, "r").read().split("\n\n")
+    )
+
+    # parse stack instructions into list of deques
+    stack_count = int(stack_input[-1].replace(" ", "")[-1])
+    stacks = [deque() for _ in range(stack_count)]
+    for line in stack_input[:-1]:
+        for i in range(stack_count):
+            crate = line[(4 * i) + 1]
+            if crate != " ":
+                stacks[i].appendleft(crate)
+
+    # parse move instructions into list of tuples (count, from_stack, to_stack)
+    MOVES_REGEXP = re.compile(r"move (\d+) from (\d+) to (\d+)")
+    moves = [
+        tuple(map(int, MOVES_REGEXP.match(line.strip()).groups()))
+        for line in moves_input
+    ]
+    return stacks, moves
+
+
+stacks, moves = parse_supply_input("input.txt")
+
+for count, from_stack, to_stack in moves:
+    for _ in range(count):
+        stacks[to_stack - 1].append(stacks[from_stack - 1].pop())
+
+
+top_crates = "".join([stack[-1] for stack in stacks])
+print(top_crates)

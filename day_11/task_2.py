c6fc4314983d6ee150f5183434f24ab8b6f61236
Philipp Kochanski <kochanskip@outlook.com>
2022-12-11 20:02:53 +0100

    Solution for Day 11

---
+++ day_11/task_2.py
@@ -0,0 +1,46 @@
+import re
+from operator import add, mul
+from collections import defaultdict
+from math import lcm
+
+RE_MONKEY_NUM = re.compile(r"Monkey (\d+):")
+RE_STARTING_ITEMS = re.compile(r"Starting items: (.+?)\n")
+RE_OPERATION = re.compile(r"Operation: new = old (\+|\*) (old|\d+)")
+RE_TEST = re.compile(r"Test: divisible by (\d+)\n.*(\d+)\n.*(\d+)")
+
+
+def generate_function(operation, param):
+    op = mul if operation == "*" else add
+    return lambda x: op(x, x if param == "old" else int(param))
+
+
+monkey_info = open("input.txt").read().split("\n\n")
+monkeys = []
+items = {}
+operations = {}
+tests = {}
+
+for info in monkey_info:
+    monkey = int(RE_MONKEY_NUM.search(info).group(1))
+    monkeys.append(monkey)
+    items[monkey] = list(map(int, RE_STARTING_ITEMS.search(info).group(1).split(", ")))
+    operations[monkey] = generate_function(*RE_OPERATION.search(info).groups())
+    tests[monkey] = tuple((map(int, RE_TEST.search(info).groups())))
+
+monkey_lcm = lcm(*[test[0] for test in tests.values()])
+inspections = defaultdict(int)
+rounds = 10000
+
+for r in range(rounds):
+    for m in monkeys:
+        while items[m]:
+            inspections[m] += 1
+            # get lcm of all divisable by values so that using mod on new_item won't affect the calculation of the new monkey
+            new_item = operations[m](items[m].pop(0)) % monkey_lcm
+            div, true_monkey, false_monkey = tests[m]
+            new_monkey = false_monkey if new_item % div else true_monkey
+            items[new_monkey].append(new_item)
+
+
+monkey_business = mul(*sorted(inspections.values(), reverse=True)[:2])
+print(monkey_business)
